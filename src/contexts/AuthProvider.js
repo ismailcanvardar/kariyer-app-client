aea45cea4e18a6f9ac8e2ea66c8aa65c10c70a52
mythi <ismailcanvardar@gmail.com>
2020-09-27 21:30:17 +0300

    Login screen is done.

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -5,6 +5,8 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(null);
+  const [userToken, setUserToken] = useState(null);
+  const [userRole, setUserRole] = useState(null);
 
   const getUser = async () => {
     const user = await AsyncStorage.getItem("user");
@@ -13,7 +15,23 @@ const AuthProvider = ({ children }) => {
       setIsUserLoggedIn(false);
     } else {
       setIsUserLoggedIn(true);
+      const savedUser = JSON.parse(user);
+      setUserToken(saveUser.token);
+      setUserRole(saveUser.role);
     }
+  };
+
+  const saveUser = async (token, role) => {
+    const user = { token, role };
+    await AsyncStorage.setItem("user", user.toString());
+    setIsUserLoggedIn(true);
+    setUserToken(token);
+    setUserRole(role);
+  };
+
+  const logoutUser = async () => {
+    await AsyncStorage.removeItem("user");
+    setIsUserLoggedIn(false);
   };
 
   useEffect(() => {
@@ -21,7 +39,7 @@ const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isUserLoggedIn }}>
+    <AuthContext.Provider value={{ isUserLoggedIn, saveUser, logoutUser }}>
       {children}
     </AuthContext.Provider>
   );
--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,12 +1,193 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import Constants from "expo-constants";
+import {
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
+import {
+  ButtonGroup,
+  Text,
+  Input,
+  Divider,
+  Button,
+} from "react-native-elements";
+import { Colors, Spacing, Typography, Mixins } from "../../styles/index";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { AuthContext } from "../../contexts/AuthProvider";
+import { loginEmployee } from "../../api/employees";
+import { loginEmployer, registerEmployer } from "../../api/employers";
 
-function LoginScreen() {
+const buttons = ["İşveren", "Çalışan"];
+
+function LoginScreen({ navigation }) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+
+  const { saveUser } = useContext(AuthContext);
+
+  const updateSelectedIndex = (index) => {
+    setSelectedIndex(index);
+  };
+
+  const removeInputTexts = () => {
+    setEmail("");
+    setPassword("");
+    setLoginError(false);
+  };
+
+  const login = () => {
+    if (selectedIndex === 0) {
+      loginEmployer(
+        { email, password },
+        (data) => {
+          saveUser(data.data.token, "employer");
+          console.log(data.data.token);
+        },
+        (err) => {
+          console.log(err.response.data);
+          if (err.response.data.title === "Wrong Credentials") {
+            setLoginError(true);
+          }
+        }
+      );
+    } else if (selectedIndex === 1) {
+      loginEmployee(
+        { email, password },
+        (data) => {
+          saveUser(data.data.token, "employee");
+          console.log(data.data.token);
+        },
+        (err) => {
+          console.log(err.response.data);
+          if (err.response.data.title === "Wrong Credentials") {
+            setLoginError(true);
+          }
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    removeInputTexts();
+  }, [selectedIndex]);
+
   return (
-    <View>
-      <Text>Hi</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        {selectedIndex === -1 && (
+          <View style={styles.headingContainer}>
+            <Text
+              h4
+              style={{
+                color: Colors.SECONDARY,
+                fontWeight: Typography.FONT_WEIGHT_BOLD,
+              }}
+            >
+              Lütfen üyelik tipini seçiniz.
+            </Text>
+          </View>
+        )}
+        <View style={styles.buttonGroupContainer}>
+          <ButtonGroup
+            buttons={buttons}
+            selectedIndex={selectedIndex}
+            containerStyle={{ height: Spacing.SCALE_32 }}
+            onPress={updateSelectedIndex}
+          />
+        </View>
+        {selectedIndex !== -1 && (
+          <View style={styles.registerContainer}>
+            <View style={styles.registerBlock}>
+              <Input
+                value={email}
+                placeholder="Eposta"
+                onChangeText={(text) => setEmail(text)}
+                autoCapitalize="none"
+                errorMessage={
+                  loginError === true ? "Eposta veya şifreniz yanlış." : ""
+                }
+              />
+              <Input
+                value={password}
+                placeholder="Şifre"
+                secureTextEntry={true}
+                onChangeText={(text) => setPassword(text)}
+                autoCapitalize="none"
+                textContentType="oneTimeCode"
+              />
+              <View style={styles.buttonContainer}>
+                <Button
+                  onPress={() => login()}
+                  icon={
+                    <Icon
+                      name="sign-in"
+                      size={15}
+                      color="white"
+                      style={{ marginRight: 5 }}
+                    />
+                  }
+                  buttonStyle={{
+                    marginHorizontal: Spacing.SCALE_8,
+                    borderColor: Colors.PRIMARY,
+                  }}
+                  containerStyle={{
+                    paddingBottom: 2,
+                  }}
+                  title={
+                    selectedIndex == 0
+                      ? "İşveren Olarak Giriş Yap"
+                      : "Çalışan Olarak Giriş Yap"
+                  }
+                />
+                <Button
+                  onPress={() => navigation.navigate("Register")}
+                  buttonStyle={{
+                    marginHorizontal: Spacing.SCALE_8,
+                    borderColor: Colors.PRIMARY,
+                  }}
+                  containerStyle={{
+                    paddingBottom: 2,
+                  }}
+                  titleStyle={{
+                    color: Colors.PRIMARY,
+                  }}
+                  type="clear"
+                  title="Kayıtlı değil misiniz?"
+                />
+              </View>
+            </View>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.BACKGROUND,
+    paddingTop: Constants.statusBarHeight,
+    justifyContent: "center",
+  },
+  headingContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: Spacing.SCALE_18,
+  },
+  registerContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  registerBlock: {
+    flex: 1,
+    width: Mixins.WINDOW_WIDTH / 1.2,
+  },
+});
